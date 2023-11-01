59d3e94fc544c2e24d2d1ebe1b3cec23d7b502f5
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-10-31 20:35:39 -0500

    create function that gets all days 1 year before and after

--- src/App.jsx
+++ src/App.jsx
@@ -42,16 +42,38 @@ function App() {
     }
   };
 
-  const getMonths = () => {
-    const today = new Date();
-    const theMonth = today.getMonth();
-    const oneMonthAgo = today.setMonth(today.getMonth() - 1);
+  // const getMonths = () => {
+  //   const today = new Date();
+  //   const theMonth = today.getMonth();
+  //   const oneMonthAgo = today.setMonth(today.getMonth() - 1);
 
-    console.log(today);
-    console.log(theMonth);
-    console.log(strMonth(theMonth));
-    console.log(oneMonthAgo);
+  //   console.log(today);
+  //   console.log(theMonth);
+  //   console.log(strMonth(theMonth));
+  //   console.log(oneMonthAgo);
+  // };
+
+  const yearDist = () => {
+    const days = [];
+    const date = new Date();
+    console.log(date);
+
+    for (let i = 365; i > 0; i--) {
+      const date = new Date();
+      const prevDate = new Date(date);
+      prevDate.setDate(prevDate.getDate() - i);
+      days.push(prevDate);
+    }
+    days.push(date);
+    for (let i = 1; i < 365; i++) {
+      const date = new Date();
+      const newerDate = new Date(date);
+      newerDate.setDate(newerDate.getDate() + i);
+      days.push(newerDate);
+    }
+    console.log(days[366].toDateString());
   };
+  yearDist();
 
   getMonths();
   return (
