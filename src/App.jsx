6eb547bf443c53483c080813336fe89cf0785dfa
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-11-26 20:56:54 -0600

    removed excess margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 import Calendar from "./components/Calendar";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBJNZyqz24bFkhX70Z2YBBuylgmF_dD-c",
+    authDomain: "booking-calendar-8fd29.firebaseapp.com",
+    projectId: "booking-calendar-8fd29",
+    storageBucket: "booking-calendar-8fd29.appspot.com",
+    messagingSenderId: "319323494818",
+    appId: "1:319323494818:web:be821bbf1f560f3e3dfafb",
+    measurementId: "G-VCKZ35LQMP",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   return (
     <>
       <div
